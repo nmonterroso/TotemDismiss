79743ff710f8449278e30540ad537592290169e2
nmonterroso <nmonterroso@users.noreply.github.com>
2019-10-19 23:54:45 -0700

    use textures!

--- main.lua
+++ main.lua
@@ -1,29 +1,33 @@
-local earth = 'earth'
 local fire = 'fire'
+local earth = 'earth'
 local water = 'water'
 local air = 'air'
 
 TotemDismiss = {
   totems = {
     [fire] = {
-      id = 1
+      id = 1,
+      icon = 'Interface/ICONS/Spell_Fire_Fire',
     },
     [earth] = {
-      id = 2
+      id = 2,
+      icon = 'Interface/ICONS/INV_Ore_Iron_01',
     },
     [water] = {
-      id = 3
+      id = 3,
+      icon = 'Interface/ICONS/INV_Stone_02',
     },
     [air] = {
-      id = 4
+      id = 4,
+      icon = 'Interface/ICONS/Spell_Nature_EarthBind',
     }
   },
-  order = {earth, fire, water, air}
+  order = {earth, fire, water, air},
 }
 
 -- TODO: read from config?
-local width = 64
-local height = 64
+local width = 32
+local height = 32
 
 function TotemDismiss:onLogin()
   local _, classFilename = UnitClass("player")
@@ -38,39 +42,34 @@ function TotemDismiss:onLogin()
 end
 
 function createButton(name, def, prevFrame, width, height)
-  local frame = CreateFrame("Button", "TotemDismissButton"..name, UIParent, "SecureActionButtonTemplate")
+  local frame = CreateFrame("Button", "TotemDismissButton_"..name, UIParent, "SecureActionButtonTemplate")
 
   if prevFrame == nil then
-    print('frame before '..name..' is nil!')
-    frame:SetPoint("CENTER", mainframe, "CENTER", 0, 0)
+    frame:SetPoint("CENTER", mainframe, "CENTER", -1.5*width, 0)
   else
-    print('not nil! positioning relative...')
     frame:SetPoint("LEFT", prevFrame, "RIGHT")
   end
 
+  -- TODO: set strata?
   frame:SetWidth(width)
   frame:SetHeight(height)
 
-  frame:SetText(name)
-  frame:SetNormalFontObject("GameFontNormal")
-
   local ntex = frame:CreateTexture()
-  ntex:SetTexture("Interface/Buttons/UI-Panel-Button-Up")
-  ntex:SetTexCoord(0, 0.625, 0, 0.6875)
+  ntex:SetTexture(def.icon)
   ntex:SetAllPoints()
   frame:SetNormalTexture(ntex)
 
-  local htex = frame:CreateTexture()
-  htex:SetTexture("Interface/Buttons/UI-Panel-Button-Highlight")
-  htex:SetTexCoord(0, 0.625, 0, 0.6875)
-  htex:SetAllPoints()
-  frame:SetHighlightTexture(htex)
-
-  local ptex = frame:CreateTexture()
-  ptex:SetTexture("Interface/Buttons/UI-Panel-Button-Down")
-  ptex:SetTexCoord(0, 0.625, 0, 0.6875)
-  ptex:SetAllPoints()
-  frame:SetPushedTexture(ptex)
+--  local htex = frame:CreateTexture()
+--  htex:SetTexture("Interface/Buttons/UI-Panel-Button-Highlight")
+--  htex:SetTexCoord(0, 0.625, 0, 0.6875)
+--  htex:SetAllPoints()
+--  frame:SetHighlightTexture(htex)
+--
+--  local ptex = frame:CreateTexture()
+--  ptex:SetTexture("Interface/Buttons/UI-Panel-Button-Down")
+--  ptex:SetTexCoord(0, 0.625, 0, 0.6875)
+--  ptex:SetAllPoints()
+--  frame:SetPushedTexture(ptex)
 
   frame:SetAttribute("*type1", "destroytotem")
   frame:SetAttribute("*totem-slot*", def.id)
