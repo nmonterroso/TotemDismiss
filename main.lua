bb562d3f964617fd2d57b01f78365db8bf50d626
nmonterroso <nmonterroso@users.noreply.github.com>
2019-10-20 17:07:13 -0700

    add option to scale the bar

--- main.lua
+++ main.lua
@@ -12,6 +12,7 @@ function TotemDismiss:OnInitialize()
 
   self.db = LibStub("AceDB-3.0"):New("TotemDismissDB", TotemDismissDefaultVariables)
   self.config = nil
+  self.optionFrame = TotemDismissOptions:setup()
   self.totemOrder = {earth, fire, water, air}
   self.totems = {
     [fire] = {
@@ -88,9 +89,7 @@ function TotemDismiss:initTotems()
   for _, type in ipairs(self.totemOrder) do
     local totem = self.totems[type]
 
-    if not self.totems[type].isReady then
-      self:initTotem(totem.id, type, lastAnchor)
-    end
+    self:initTotem(totem.id, type, lastAnchor)
 
     if totem.isReady then
       lastAnchor = totem.button
@@ -100,9 +99,9 @@ end
 
 function TotemDismiss:initTotem(id, type, anchor)
   local button = self:createButton(id, type, anchor)
-  local iconTexture = self:createIconTexture(button)
-  local overlay = self:createOverlay(button)
-  local cooldown = self:createCooldown(button)
+  local iconTexture = self:createIconTexture(id, button)
+  local overlay = self:createOverlay(id, button)
+  local cooldown = self:createCooldown(id, button)
 
   button:SetNormalTexture(iconTexture)
   button:Show()
@@ -160,7 +159,13 @@ function TotemDismiss:enable(id)
 end
 
 function TotemDismiss:createButton(id, type, anchor)
-  local button = CreateFrame("Button", "TotemDismissButton_"..type, UIParent, "SecureActionButtonTemplate")
+  local totem = self:getTotem(id)
+  local button
+  if totem.isReady then
+    button = totem.button
+  else
+    button = CreateFrame("Button", "TotemDismissButton_"..type, UIParent, "SecureActionButtonTemplate")
+  end
 
   if anchor == nil then
     button:SetPoint(self.configHelper:GetInitialButtonPoint())
@@ -169,16 +174,21 @@ function TotemDismiss:createButton(id, type, anchor)
   end
 
   -- TODO: set strata?
-  button:SetWidth(self.config.buttonWidth)
-  button:SetHeight(self.config.buttonHeight)
+  button:SetWidth(self.config.buttonWidth*self.config.scale)
+  button:SetHeight(self.config.buttonHeight*self.config.scale)
   button:SetAttribute("*type1", "destroytotem")
   button:SetAttribute("*totem-slot*", id)
 
   return button
 end
 
-function TotemDismiss:createCooldown(button)
-  local cooldown = CreateFrame("Cooldown", nil, button, "CooldownFrameTemplate")
+function TotemDismiss:createCooldown(id, button)
+  local totem = self:getTotem(id)
+  local cooldown = totem.cooldown
+  if not totem.isReady then
+    cooldown = CreateFrame("Cooldown", nil, button, "CooldownFrameTemplate")
+  end
+
   cooldown:SetReverse(true)
   cooldown:SetDrawEdge(false)
   cooldown:SetPoint("TOPLEFT", 1, -1)
@@ -187,19 +197,31 @@ function TotemDismiss:createCooldown(button)
   return cooldown
 end
 
-function TotemDismiss:createIconTexture(button)
-  local texture = button:CreateTexture()
-  texture:SetAllPoints()
+function TotemDismiss:createIconTexture(id, button)
+  local totem = self:getTotem(id)
+  local texture
+  if totem.isReady then
+    texture = totem.iconTexture
+  else
+    texture = button:CreateTexture()
+  end
 
+  texture:SetAllPoints()
   return texture
 end
 
-function TotemDismiss:createOverlay(button)
-  local overlay = button:CreateTexture(nil, "OVERLAY")
+function TotemDismiss:createOverlay(id, button)
+  local totem = self:getTotem(id)
+  local overlay
+  if totem.isReady then
+    overlay = totem.overlay
+  else
+    overlay = button:CreateTexture(nil, "OVERLAY")
+  end
+
   overlay:SetPoint("TOPLEFT", 1, -1)
   overlay:SetPoint("BOTTOMRIGHT", -1, 1)
   overlay:SetColorTexture(0, 0, 0, .55)
-  overlay:Show()
 
   return overlay
 end
---
+++ options.lua
@@ -0,0 +1,41 @@
+local options = {
+  name = 'TotemDismiss',
+  type = "group",
+  args = {
+    locked = {
+      name = "Locked",
+      desc = "Prevents the bar from being moved",
+      type = "toggle",
+      set = function(info, val)
+        TotemDismiss.config.locked = val
+        TotemDismiss:initTotems()
+      end,
+      get = function(info) return TotemDismiss.config.locked end
+    },
+    scale = {
+      name = "Scale",
+      desc = "Button scale",
+      type = "range",
+      softMin = 0.1,
+      softMax = 1.9,
+      set = function(info, val)
+        if val < 0.1 then
+          val = 0.1
+        elseif val > 1.9 then
+          val = 1.9
+        end
+
+        TotemDismiss.config.scale = val
+        TotemDismiss:initTotems()
+      end,
+      get = function(info) return TotemDismiss.config.scale end
+    }
+  }
+}
+
+TotemDismissOptions = {}
+function TotemDismissOptions:setup()
+  LibStub("AceConfig-3.0"):RegisterOptionsTable("TotemDismiss", options, {"totemdismiss"})
+  local optionFrame = LibStub("AceConfigDialog-3.0"):AddToBlizOptions("TotemDismiss", "Totem Dismiss")
+  return optionFrame
+end
